a216fa1c3fe032aea04cde2db07bb2aeadc31c24
Ger Hobbelt <ger@hobbelt.com>
2025-07-13 17:51:58 +0200

    Create scoped/multiple-container-instances/mci-main.cpp

---
+++ examples/scoped/multiple-container-instances/mci-main.cpp
@@ -0,0 +1,4 @@
+
+// showcases the scenario where all parameters are members of an application class which is instanced multiple times.
+//
+// the registered set identifies the parent of each parameter so each can be configured individually and independently. 
