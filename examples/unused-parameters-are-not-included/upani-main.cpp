4862ceea01c53516537cae911bd176e35b904e0c
Ger Hobbelt <ger@hobbelt.com>
2025-07-13 17:39:20 +0200

    Create unused-parameters-are-not-included/upani-main.cpp

---
+++ examples/unused-parameters-are-not-included/upani-main.cpp
@@ -0,0 +1,10 @@
+
+// showcase the scenario where parameters, which
+// are otherwise unused in the application, 
+// will be discarded at the linker phase.
+//
+// this can be observed by inspecting the list of registered parameters,
+// as produced by the libparameters manager class/singleton.
+//
+
+
