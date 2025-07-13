47f4fb6f1d5ff1cda083ab6b27b4581436fb431d
Ger Hobbelt <ger@hobbelt.com>
2025-07-13 17:54:56 +0200

    Create advanced/activity-callbacks/ac-main.cpp

---
+++ examples/advanced/activity-callbacks/ac-main.cpp
@@ -0,0 +1,3 @@
+
+// showcases the scenario where we use advanced features of the libparameters library including userland defined activity callbacks providing custom processing of parameter activities & statistics.
+
