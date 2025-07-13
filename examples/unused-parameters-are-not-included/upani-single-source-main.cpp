ff9a5e0389375af390a1cf5bfa39e27e3453988c
Ger Hobbelt <ger@hobbelt.com>
2025-07-13 17:43:27 +0200

    Create upani-single-source-main.cpp

---
+++ examples/unused-parameters-are-not-included/upani-single-source-main.cpp
@@ -0,0 +1,10 @@
+// ditto, but now we have all *defined* parameters
+// in a single source/compile unit 
+// ==>
+// this showcases whether your compiler/settings
+// split a single source into multiple 
+// independent objects/particles for the linker
+// to include or discard in the final application.
+
+
+
